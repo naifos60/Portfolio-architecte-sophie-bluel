475c87ed556aef73c5cc7c065387abf0cea39584
naifos60 <naifos60@hotmail.com>
2023-04-28 11:09:34 +0200

    feature post login

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,13 +1,12 @@
-/********** variables ********/
+/********** function ********/
+async function logUser(){
     const emailUser = document.querySelector("#mail_log").value;
     const passwordUser = document.querySelector("#password_log").value;
     const infoUser = {
         email: emailUser,
         password: passwordUser
     };
-    const identify = JSON.stringify(infoUser);
-/********** function ********/
-async function logUser(){    
+    const identify = JSON.stringify(infoUser);      
     await fetch("http://localhost:5678/api/users/login",{
         method: "POST",
         headers: {
@@ -16,14 +15,15 @@ async function logUser(){
         },
         body: identify
     }).then(response => {
-        if(!response.ok){
-            document.querySelector(".error_message").style.display = "block";
+        if(response.ok){
+            window.location.assign("./edit.html");
         }else{
-            return response.json;
+            document.querySelector(".error_message").style.display = "block";
+           console.log(response); 
         }
     })
     console.log(identify);
-}
+};
 /********* ajout listener submit *********/
 document.querySelector(".submit_form").addEventListener("click", function(e){
     e.preventDefault();
